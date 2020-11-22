2205beda8e959192e7aced048a2c370a32e7d3d6
Shivam Jadhav <34097420+Flash-Shivam@users.noreply.github.com>
2020-11-22 13:48:38 +0530

    N queens solved , Pending Allsat , ocamlgraph

--- A2.ml
+++ A2.ml
@@ -1,25 +1,101 @@
 open Formula;;
 open Bdd;;
 
-(* 	The type of n_queen is int -> string list, where int is the 
-	size of board. The solution is represented as list of strings, 
-	where each string denotes the position of a queen in the solution. 
-	The position is a string 'c' (lower case c without quotes) 
-	appended with two single digit integers i and j, where i and j 
-	are row and column numbers respectively, starting from 0. 
-	For example, the string for cell in row 0 and column 4 should be c04. 
+(* 	The type of n_queen is int -> string list, where int is the
+	size of board. The solution is represented as list of strings,
+	where each string denotes the position of a queen in the solution.
+	The position is a string 'c' (lower case c without quotes)
+	appended with two single digit integers i and j, where i and j
+	are row and column numbers respectively, starting from 0.
+	For example, the string for cell in row 0 and column 4 should be c04.
 *)
-let n_queen board_size = raise BDD.Not_implemented ;;
+
 
 (*	The type of  knight is int -> int -> int -> string list, where
 	first int is board size, second and third ints represent the
-	row and column number (starting from 0) respectively of 
+	row and column number (starting from 0) respectively of
 	initial position of the knight on the board
 	The output to this function should be a sequence of strings.
-	The first element in the sequence should be cell name 
-	corresponding to the initial position of the knight. 
-	Each subsequent string in the sequence should represent the 
-	next cell visited by the knight. 
+	The first element in the sequence should be cell name
+	corresponding to the initial position of the knight.
+	Each subsequent string in the sequence should represent the
+	next cell visited by the knight.
 *)
-let knight board_size init_row init_col = raise BDD.Not_implemented ;;	
+let knight board_size init_row init_col = raise BDD.Not_implemented ;;
 
+
+let new_table = Hashtbl.create 1000;;
+let rec add_to_tab size = if size = 0 then Hashtbl.add new_table size "0" else let r = Hashtbl.add new_table size (string_of_int (size)) in add_to_tab (size-1);;
+let do_some size = let p = Hashtbl.clear new_table in add_to_tab (size*size-1)
+;;
+let rec row_cond row end_r = if row = end_r then Program.Variable(Hashtbl.find new_table row) else Program.OprBinary(Program.OR,Program.Variable(Hashtbl.find new_table row),row_cond (row-1) end_r);;
+
+let rec make_Expr_one index1 index2 size run_iter = if run_iter = 1  then (if (run_iter = index2) then Program.Constant(true) else Program.OprUnary(Program.NOT,Program.Variable(Hashtbl.find new_table ( (index1-1)*size + run_iter -1 ))) ) else (if (run_iter = index2 )then Program.OprBinary(Program.AND,Program.Constant(true),make_Expr_one index1 index2 size (run_iter-1))  else Program.OprBinary(Program.AND,Program.OprUnary(Program.NOT,Program.Variable(Hashtbl.find new_table ( (index1-1)*size + run_iter -1 ) )),make_Expr_one index1 index2 size (run_iter-1) ));;
+
+let rec make_Expr_two index1 index2 size run_iter = if run_iter = 1  then (if (run_iter = index1) then Program.Constant(true) else Program.OprUnary(Program.NOT,Program.Variable(Hashtbl.find new_table ( (run_iter-1)*size + index2 -1 )) )) else (if (run_iter = index1 )then Program.OprBinary(Program.AND,Program.Constant(true),make_Expr_two index1 index2 size (run_iter-1))  else Program.OprBinary(Program.AND,Program.OprUnary(Program.NOT,Program.Variable(Hashtbl.find new_table ( (run_iter-1)*size + index2 -1 ) )),make_Expr_two index1 index2 size (run_iter-1) ));;
+
+let rec make_Expr_three index1 index2 size run_iter = if run_iter = 1  then (if (run_iter = index1 || ( run_iter <=0 || run_iter > size || (run_iter+index2-index1<=0) || (run_iter+index2-index1>size) )) then Program.Constant(true) else Program.OprUnary(Program.NOT,Program.Variable(Hashtbl.find new_table ( (run_iter-1)*size + index2-index1+run_iter -1 )) )) else (if (run_iter = index1 ) then Program.OprBinary(Program.AND,Program.Constant(true),make_Expr_three index1 index2 size (run_iter-1))  else ( if ( run_iter <=0 || run_iter > size || (run_iter+index2-index1<=0) || (run_iter+index2-index1>size)  ) then Program.OprBinary(Program.AND,Program.Constant(true),make_Expr_three index1 index2 size (run_iter-1) ) else Program.OprBinary(Program.AND,Program.OprUnary(Program.NOT,Program.Variable(Hashtbl.find new_table ( (run_iter-1)*size + index2-index1+run_iter -1 ) )),make_Expr_three index1 index2 size (run_iter-1) )));;
+
+let rec make_Expr_four index1 index2 size run_iter = if run_iter = 1  then (if (run_iter = index1 || ( run_iter <=0 || run_iter > size || (-run_iter+index2+index1<=0) || (-run_iter+index2+index1>size)  )) then Program.Constant(true) else Program.OprUnary(Program.NOT,Program.Variable(Hashtbl.find new_table ( (run_iter-1)*size + index2+index1-run_iter -1 )) )) else (if (run_iter = index1 ) then Program.OprBinary(Program.AND,Program.Constant(true),make_Expr_four index1 index2 size (run_iter-1))  else ( if ( run_iter <=0 || run_iter > size || (-run_iter+index2+index1<=0) || (-run_iter+index2+index1>size)  ) then Program.OprBinary(Program.AND,Program.Constant(true),make_Expr_four index1 index2 size (run_iter-1) ) else Program.OprBinary(Program.AND,Program.OprUnary(Program.NOT,Program.Variable(Hashtbl.find new_table ( (run_iter-1)*size + index2+index1-run_iter -1 ) )),make_Expr_four index1 index2 size (run_iter-1) )));;
+
+let m_ex_1 index1 index2 size = Program.OprBinary(Program.IFTHEN,Program.Variable(Hashtbl.find new_table ((index1-1)*size+index2-1)  ),make_Expr_one index1 index2 size size);;
+
+let m_ex_2 index1 index2 size = Program.OprBinary(Program.IFTHEN,Program.Variable(Hashtbl.find new_table ((index1-1)*size+index2-1)  ),make_Expr_two index1 index2 size size);;
+
+let m_ex_3 index1 index2 size = Program.OprBinary(Program.IFTHEN,Program.Variable(Hashtbl.find new_table ((index1-1)*size+index2-1)  ),make_Expr_three index1 index2 size size);;
+
+let m_ex_4 index1 index2 size = Program.OprBinary(Program.IFTHEN,Program.Variable(Hashtbl.find new_table ((index1-1)*size+index2-1)  ),make_Expr_four index1 index2 size size);;
+
+let rec f_iter index1 index2 size l = if index2 = size then (if index1 == size then l@[(index1,index2)] else f_iter (index1+1) (1) size l@[(index1,index2)] ) else f_iter (index1) (index2+1) size l@[(index1,index2)];;
+
+let rec f_comp_one l size g = match l with
+| [] -> g
+| head :: tail -> match head with (a,b) -> f_comp_one tail size g@[m_ex_1 (a) (b) size];;
+
+let f_one size = f_comp_one (f_iter (1) (1) size ([])) size ([]);;
+
+let rec f_comp_two l size g = match l with
+| [] -> g
+| head :: tail -> match head with (a,b) -> f_comp_two tail size g@[m_ex_2 (a) (b) size];;
+
+let f_two size = f_comp_two (f_iter (1) (1) size ([])) size ([]);;
+
+let rec f_comp_three l size g = match l with
+| [] -> g
+| head :: tail -> match head with (a,b) -> f_comp_three tail size g@[m_ex_3 (a) (b) size];;
+
+let f_three size = f_comp_three (f_iter (1) (1) size ([])) size ([]);;
+
+let rec f_comp_four l size g = match l with
+| [] -> g
+| head :: tail -> match head with (a,b) -> f_comp_four tail size g@[m_ex_4 (a) (b) size];;
+
+let f_four size = f_comp_four (f_iter (1) (1) size ([])) size ([]);;
+
+let rec f_comp_five size l iter_x = if (iter_x = size-1) then l@[row_cond ((iter_x+1)*size-1) (iter_x*size)] else (f_comp_five (size) (l@[row_cond ((iter_x+1)*size-1) (iter_x*size)]) (iter_x+1));;
+
+let f_five size = f_comp_five size ([]) (0);;
+
+let get_all_bool_expr size = (f_one size)@(f_two size)@(f_three size)@(f_four size)@(f_five size);;
+
+let rec string_list_x l iter_x size = if iter_x = size-1 then l@[string_of_int (size-1)] else string_list_x (l@[string_of_int (iter_x)]) (iter_x+1) (size);;
+
+let rec gen_order size = string_list_x  ([]) (0) (size*size);;
+
+let rec and_build l = match l with
+| [x] -> x
+| head :: tail -> Program.OprBinary(Program.AND,head,and_build tail);;
+
+let solve_count size = let p = do_some size in BDD.sat_count (BDD.bddFromExpr (and_build (get_all_bool_expr size)) (gen_order size));;
+
+let solve size = let p = do_some size in BDD.any_sat (BDD.bddFromExpr (and_build (get_all_bool_expr size)) (gen_order size));;
+
+let rec conv_to_res l g size = match l with
+| [] -> g
+| head :: tail -> conv_to_res (tail) (g@[ String.concat (string_of_int(int_of_string (head) / size)) ["c"; string_of_int(int_of_string (head) mod size)]  ]) size
+;;
+
+
+let final_sol size = conv_to_res (solve size) ([]) size;;
+
+let n_queen board_size = final_sol board_size ;;
--- bdd.ml
+++ bdd.ml
@@ -3,17 +3,117 @@ module BDD = struct
 		type sat_assignment = string list
 
 		(* define the type robdd in whatever way you want  *)
-		type robdd = bool list ;;
+		type robdd = int ;;
 
 		exception Not_implemented
 
+		exception Cant_be_evaluated
+		exception No_valid_assingment
+		let rec feval bexpr bassign = match bexpr with
+		| Program.Constant(false) -> 0
+		| Program.OprUnary(Program.NOT,a) -> if ((feval a bassign) =1) then 0 else 1
+		| Program.Variable(x) -> if (List.mem x bassign) then 1 else raise Cant_be_evaluated
+		| Program.Constant(true) -> 1
+		| Program.OprBinary(Program.AND,a,b) -> if ( (feval a bassign) = 0 || (feval b bassign) = 0) then 0 else (feval a bassign) * (feval b bassign)
+		| Program.OprBinary(Program.OR,a,b) -> if ( (feval a bassign) = 1 || (feval b bassign) = 1) then 1 else 0;
+		| Program.OprBinary(Program.IFTHEN,a,b) -> if((feval a bassign) = 0 || (feval b bassign) = 1) then 1 else 0
+		| Program.OprBinary(Program.IFF,a,b) -> if((feval a bassign) = (feval b bassign)) then 1 else 0
+		| Program.OprTernary(Program.IFTHENELSE,a,b,c) -> if((feval a bassign) = 1) then (feval b bassign) else (feval c bassign)
+		;;
 
-		let bddFromExpr bexpr order = raise Not_implemented;;
+		let rec feval_prime bexpr bassign = match bexpr with
+		| Program.Constant(false) -> 0
+		| Program.OprUnary(Program.NOT,a) -> if ((feval_prime a bassign) =1) then 0 else 1
+		| Program.Variable(x) -> if (List.mem x bassign) then 1 else 0
+		| Program.Constant(true) -> 1
+		| Program.OprBinary(Program.AND,a,b) -> if ( (feval_prime a bassign) = 0 || (feval_prime b bassign) = 0) then 0 else 1
+		| Program.OprBinary(Program.OR,a,b) -> if ( (feval_prime a bassign) = 1 || (feval_prime b bassign) = 1) then 1 else 0;
+		| Program.OprBinary(Program.IFTHEN,a,b) -> if((feval_prime a bassign) = 0 || (feval_prime b bassign) = 1) then 1 else 0
+		| Program.OprBinary(Program.IFF,a,b) -> if((feval_prime a bassign) = (feval_prime b bassign)) then 1 else 0
+		| Program.OprTernary(Program.IFTHENELSE,a,b,c) -> if((feval_prime a bassign) = 1) then (feval_prime b bassign) else (feval_prime c bassign)
+		;;
 
 
-		let sat_count bdd = raise Not_implemented ;;
-		let all_sat bdd = raise Not_implemented ;;
-		let any_sat bdd =  raise Not_implemented ;;
+		let safe_fval bexpr = try feval bexpr ([]) with Cant_be_evaluated -> -1;;
+
+		let rec substitute_x bexpr var_x bool_x = match bexpr with
+		| Program.Variable(x) -> if (x = var_x) then (if bool_x then Program.Constant(true) else Program.Constant(false)) else Program.Variable(x)
+		| Program.OprUnary(Program.NOT,a) -> Program.OprUnary(Program.NOT,substitute_x a var_x bool_x)
+		| Program.OprBinary(z,a,b) -> Program.OprBinary(z,substitute_x a var_x bool_x,substitute_x b var_x bool_x)
+		| Program.OprTernary(Program.IFTHENELSE,a,b,c) -> Program.OprTernary(Program.IFTHENELSE,substitute_x a var_x bool_x,substitute_x b var_x bool_x,substitute_x c var_x bool_x)
+		| z -> z;;
+
+
+		let rec conv_ifte bexpr = match bexpr with
+		| Program.OprUnary(Program.NOT,a) -> Program.OprTernary(Program.IFTHENELSE,(conv_ifte a),Program.Constant(false),Program.Constant(true))
+		| Program.OprBinary(Program.AND,a,b) -> Program.OprTernary(Program.IFTHENELSE,(conv_ifte a),(conv_ifte b),Program.Constant(false))
+		| Program.OprBinary(Program.OR,a,b) -> Program.OprTernary(Program.IFTHENELSE,(conv_ifte a),Program.Constant(true),(conv_ifte b))
+		| Program.OprBinary(Program.IFTHEN,a,b) -> Program.OprTernary(Program.IFTHENELSE,(conv_ifte a),(conv_ifte b),Program.Constant(true))
+		| Program.OprBinary(Program.IFF,a,b) -> Program.OprTernary(Program.IFTHENELSE,(conv_ifte a),(conv_ifte b),(conv_ifte (Program.OprUnary(Program.NOT,b))))
+		| Program.OprTernary(Program.IFTHENELSE,a,b,c) -> Program.OprTernary(Program.IFTHENELSE,(conv_ifte a),(conv_ifte b),(conv_ifte c))
+		| a -> a
+		;;
+
+
+		let table = Hashtbl.create 1000;;
+		let table2 = Hashtbl.create 1000;;
+		let var_table = Hashtbl.create 1000;;
+
+
+
+
+		let mk index low high = if (low = high) then low else if (Hashtbl.mem table2 (index,low,high)) then Hashtbl.find table2 (index,low,high) else let p = Hashtbl.length table in let z = Hashtbl.add table (p) (index,low,high) in let y = Hashtbl.add table2 (index,low,high) (p) in (p);;
+
+
+		let rec robddFromExpr_prime bexpr order num index l = if (index > num) then feval_prime bexpr l  else match order with
+		| head :: tail -> (mk index (robddFromExpr_prime (bexpr) (tail) (num) (index+1) (l)) (robddFromExpr_prime (bexpr) (tail) (num) (index+1) (l@[Hashtbl.find var_table (index)])));;
+
+		let rec robddFromExpr bexpr order num index = if (index > num) then safe_fval bexpr else match order with
+		| head :: tail -> (mk index (robddFromExpr (substitute_x (bexpr) (head) (false)) tail num (index+1)) (robddFromExpr (substitute_x (bexpr) (head) (true)) tail num (index+1)));;
+
+		let rec power x n = if n = 0 then 1 else if n = 1 then x else x * (power x (n-1));;
+
+		let get_low node = match (Hashtbl.find table node) with
+		| (a,b,c) -> b;;
+
+		let get_high node = match (Hashtbl.find table node) with
+		| (a,b,c) -> c;;
+
+		let get_var node = match (Hashtbl.find table node) with
+		| (a,b,c) -> a;;
+
+		let rec count_sat node = if (node = 0) then 0 else if (node = 1) then 1 else (power (2) (get_var (get_low node) - (get_var node) -1 ))*(count_sat (get_low node)) + (power (2) (get_var (get_high node) - (get_var node) -1 )  )*(count_sat (get_high node));;
+
+
+		let f_count node = (power (2) ((get_var node)-1)) * (count_sat node);;
+
+
+		let rec fill_var_table order num = match order with
+		| [x] -> Hashtbl.add var_table (num) (x)
+		| head :: tail -> let p = Hashtbl.add var_table (num) (head) in fill_var_table tail (num + 1);;
+
+		let f_robdd bexpr order = let f = Hashtbl.clear var_table in let v = fill_var_table order (1) in let r = Hashtbl.clear table in let t = Hashtbl.clear table2 in let m = Hashtbl.add table (0) (List.length order+1,-1,-1) in let n = Hashtbl.add table (1) (List.length order+1,-1,-1) in robddFromExpr bexpr order (List.length order) (1);;
+
+		let f_robdd_prime bexpr order = let f = Hashtbl.clear var_table in let v = fill_var_table order (1) in let r = Hashtbl.clear table in let t = Hashtbl.clear table2 in let m = Hashtbl.add table (0) (List.length order+1,-1,-1) in let n = Hashtbl.add table (1) (List.length order+1,-1,-1) in robddFromExpr_prime bexpr order (List.length order) (1) ([]);;
+
+		let rec f_any_sat node list_l = if node = 0 then raise No_valid_assingment else if node = 1 then list_l else if ((get_low node) = 0) then f_any_sat (get_high node) (list_l@[(Hashtbl.find var_table (get_var node))]) else  f_any_sat (get_low node) (list_l);;
+
+		let f_sat node = f_any_sat node ([]);;
+
+		let rec add_var node list_l list_p = match list_l with
+		| [] -> list_p@[[Hashtbl.find var_table (node)]]
+		| head :: tail -> add_var node tail (list_p@[head@[Hashtbl.find var_table (node)]]);;
+
+		let rec f_all_sat node list_of_l = if node = 0 then (list_of_l) else if node = 1 then (list_of_l@[]) else list_of_l@(f_all_sat (get_low node) (list_of_l))@(add_var (get_var node) (f_all_sat (get_high node) list_of_l) ([]));;
+
+		let f_asat node = f_all_sat node ([]);;
+
+		let bddFromExpr bexpr order = f_robdd_prime (bexpr) order  ;;
+
+
+		let sat_count bdd = f_count bdd ;;
+		let all_sat bdd = f_asat bdd ;;
+		let any_sat bdd =  f_sat bdd ;;
 
 
 		let to_dot bdd = raise Not_implemented;;
@@ -21,78 +121,7 @@ module BDD = struct
 end  ;;
 
 (* let a = (Program.Constant true);;
-a;; *)
-exception Cant_be_evaluated
-exception No_valid_assingment
-let rec feval bexpr bassign = match bexpr with
-| Program.Constant(false) -> 0
-| Program.OprUnary(Program.NOT,a) -> if ((feval a bassign) =1) then 0 else 1
-| Program.Variable(x) -> if (List.mem x bassign) then 1 else raise Cant_be_evaluated
-| Program.Constant(true) -> 1
-| Program.OprBinary(Program.AND,a,b) -> if ( (feval a bassign) = 0 || (feval b bassign) = 0) then 0 else (feval a bassign) * (feval b bassign)
-| Program.OprBinary(Program.OR,a,b) -> if ( (feval a bassign) = 1 || (feval b bassign) = 1) then 1 else 0;
-| Program.OprBinary(Program.IFTHEN,a,b) -> if((feval a bassign) = 0 || (feval b bassign) = 1) then 1 else 0
-| Program.OprBinary(Program.IFF,a,b) -> if((feval a bassign) = (feval b bassign)) then 1 else 0
-| Program.OprTernary(Program.IFTHENELSE,a,b,c) -> if((feval a bassign) = 1) then (feval b bassign) else (feval c bassign)
-;;
-
-let safe_fval bexpr = try feval bexpr ([]) with Cant_be_evaluated -> -1;;
-
-let rec substitute_x bexpr var_x bool_x = match bexpr with
-| Program.Variable(x) -> if (x = var_x) then (if bool_x then Program.Constant(true) else Program.Constant(false)) else Program.Variable(x)
-| Program.OprUnary(Program.NOT,a) -> Program.OprUnary(Program.NOT,substitute_x a var_x bool_x)
-| Program.OprBinary(z,a,b) -> Program.OprBinary(z,substitute_x a var_x bool_x,substitute_x b var_x bool_x)
-| Program.OprTernary(Program.IFTHENELSE,a,b,c) -> Program.OprTernary(Program.IFTHENELSE,substitute_x a var_x bool_x,substitute_x b var_x bool_x,substitute_x c var_x bool_x)
-| z -> z;;
+a;;*)
 
 
-let rec conv_ifte bexpr = match bexpr with
-| Program.OprUnary(Program.NOT,a) -> Program.OprTernary(Program.IFTHENELSE,(conv_ifte a),Program.Constant(false),Program.Constant(true))
-| Program.OprBinary(Program.AND,a,b) -> Program.OprTernary(Program.IFTHENELSE,(conv_ifte a),(conv_ifte b),Program.Constant(false))
-| Program.OprBinary(Program.OR,a,b) -> Program.OprTernary(Program.IFTHENELSE,(conv_ifte a),Program.Constant(true),(conv_ifte b))
-| Program.OprBinary(Program.IFTHEN,a,b) -> Program.OprTernary(Program.IFTHENELSE,(conv_ifte a),(conv_ifte b),Program.Constant(true))
-| Program.OprBinary(Program.IFF,a,b) -> Program.OprTernary(Program.IFTHENELSE,(conv_ifte a),(conv_ifte b),(conv_ifte (Program.OprUnary(Program.NOT,b))))
-| Program.OprTernary(Program.IFTHENELSE,a,b,c) -> Program.OprTernary(Program.IFTHENELSE,(conv_ifte a),(conv_ifte b),(conv_ifte c))
-| a -> a
-;;
-
-
-let table = Hashtbl.create 1000;;
-let table2 = Hashtbl.create 1000;;
-let var_table = Hashtbl.create 1000;;
-
-
-
-
-let mk index low high = if (low = high) then low else if (Hashtbl.mem table2 (index,low,high)) then Hashtbl.find table2 (index,low,high) else let p = Hashtbl.length table in let z = Hashtbl.add table (p) (index,low,high) in let y = Hashtbl.add table2 (index,low,high) (p) in (p);;
-
-
-let rec robddFromExpr bexpr order num index = if (index > num) then safe_fval bexpr else match order with
-| head :: tail -> (mk index (robddFromExpr (substitute_x (bexpr) (head) (false)) tail num (index+1)) (robddFromExpr (substitute_x (bexpr) (head) (true)) tail num (index+1)));;
-
-let rec power x n = if n = 0 then 1 else if n = 1 then x else x * (power x (n-1));;
-
-let get_low node = match (Hashtbl.find table node) with
-| (a,b,c) -> b;;
-
-let get_high node = match (Hashtbl.find table node) with
-| (a,b,c) -> c;;
-
-let get_var node = match (Hashtbl.find table node) with
-| (a,b,c) -> a;;
-
-let rec count_sat node = if (node = 0) then 0 else if (node = 1) then 1 else (power (2) (get_var (get_low node) - (get_var node) -1 ))*(count_sat (get_low node)) + (power (2) (get_var (get_high node) - (get_var node) -1 )  )*(count_sat (get_high node));;
-
-
-let f_count node = (power (2) ((get_var node)-1)) * (count_sat node);;
-
-
-let rec fill_var_table order num = match order with
-| [x] -> Hashtbl.add var_table (num) (x)
-| head :: tail -> let p = Hashtbl.add var_table (num) (head) in fill_var_table tail (num + 1);;
-
-let f_robdd bexpr order = let f = Hashtbl.clear var_table in let v = fill_var_table order (1) in let r = Hashtbl.clear table in let t = Hashtbl.clear table2 in let m = Hashtbl.add table (0) (List.length order+1,-1,-1) in let n = Hashtbl.add table (1) (List.length order+1,-1,-1) in robddFromExpr bexpr order (List.length order) (1);;
-
-let rec f_any_sat node list_l = if node = 0 then raise No_valid_assingment else if node = 1 then list_l else if ((get_low node) = 0) then f_any_sat (get_high node) (list_l@[(Hashtbl.find var_table (get_var node))]) else  f_any_sat (get_low node) (list_l);;
-
-let f_sat node = f_any_sat node ([]);;
+(*@f_all_sat (get_high node) (add_var (get_var node) list_of_l ([]))*)
